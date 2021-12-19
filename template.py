5227743e0c68339b14646389e0d4e6c49525fa8b
David Wu <wu.david3001@gmail.com>
2021-12-20 07:32:17 +1300

    Add misc files

---
+++ template.py
@@ -0,0 +1,18 @@
+input_file = 'test.input'
+# input_file = 'inputs/day_00.input'
+
+def parse(input_file):
+    with open(input_file, 'r') as fp:
+        data = fp.read().strip().split('\n')
+
+    return data
+
+def part_a():
+    pass
+
+def part_b():
+    pass
+
+if __name__ == "__main__":
+    print(part_a())
+    print(part_b())
