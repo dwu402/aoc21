d911821d8816f2e21db743b37e358fd067dcdd40
dwu402 <dwu402@aucklanduni.ac.nz>
2021-12-23 14:56:10 +1300

    Update ignore, template

--- template.py
+++ template.py
@@ -1,8 +1,8 @@
 input_file = 'test.input'
 # input_file = 'inputs/day_00.input'
 
-def parse():
-    with open(input_file, 'r') as fp:
+def parse(datafile=input_file):
+    with open(datafile, 'r') as fp:
         data = fp.read().strip().split('\n')
 
     return data
